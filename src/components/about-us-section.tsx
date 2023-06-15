31e6a44e598ae188d62afa4d7a85199f05137823
Dominik Gronkiewicz <dominik@8bits.space>
2023-06-15 12:39:45 +0700

    feat: added link to resume

--- src/components/about-us-section.tsx
+++ src/components/about-us-section.tsx
@@ -4,7 +4,9 @@ const aboutHeader = `What is 8 Bits Space?`;
 
 const aboutBodySections = [
   `We are a company established in Estonia in November 2019, with a main focus on developing custom, high-quality software in the shortest possible time.`,
-  `Our team has Polish origins, and at the core of our business is an experienced individual who has created numerous MVPs for startups since 2017. The value we provide is amplified by an extended network of developers with whom he has collaborated on previous projects. `,
+  `Our team has Polish origins, and at the core of our business is an 
+  <a href="https://dominikgronkiewicz.com/resume/" style="text-decoration: underline; color: inherit">experienced individual</a> 
+  who has created numerous MVPs for startups since 2017. The value we provide is amplified by an extended network of developers with whom he has collaborated on previous projects. `,
   `As technology evolves, so does our company. We embrace new tools that enable us to deliver products faster without compromising on quality. Our recent focus has been on all-in-one platforms that align with our customers' goals.`,
 ];
 
