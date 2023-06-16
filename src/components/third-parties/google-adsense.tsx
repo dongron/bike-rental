4bdbe3fb3070d88cdba63c46219ea3935fd4f015
Dominik Gronkiewicz <dominik@8bits.space>
2023-06-16 14:26:52 +0700

    feat: added env variables

--- src/components/third-parties/google-adsense.tsx
+++ src/components/third-parties/google-adsense.tsx
@@ -1,15 +1,19 @@
 const GoogleAdsense = () => {
+  const clientId = process.env.NEXT_PUBLIC_GOOGLE_AD_CLIENT;
+  const isEnabledOnPageLevel =
+    process.env.NEXT_PUBLIC_GOOGLE_AD_ENABLE_PAGE_LEVEL_ADS;
+
   return (
     <>
       <script
         async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8118457789375955"
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${clientId}`}
         crossOrigin="anonymous"
       />
       <script>
         {`(adsbygoogle = window.adsbygoogle || []).push({
-          google_ad_client: "ca-pub-8118457789375955",
-          enable_page_level_ads: true
+          google_ad_client: "${clientId}",
+          enable_page_level_ads: ${isEnabledOnPageLevel}
           });`}
       </script>
     </>
