4e6141de59030d9b61a43e2ccb8ad14bd973bee5
Dominik Gronkiewicz <dominik@8bits.space>
2023-07-04 10:07:15 +0700

    fix: hero height

--- src/components/navbar/navbar-link.tsx
+++ src/components/navbar/navbar-link.tsx
@@ -8,7 +8,7 @@ interface NavbarProps {
 
 const NavbarLink: FC<NavbarProps> = ({ className, children, link }) => {
   return (
-    <li className={`list-none ${className}`}>
+    <li className={`list-none leading-5 ${className}`}>
       <a
         className="group font-bold text-stone-100 decoration-0 transition duration-300"
         href={link}
